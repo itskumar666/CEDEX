b3dfb3422043f41094dd00d3aebe546d9915f819
itskumar666 <ashutosh666be@Gmail.com>
2024-08-07 00:40:09 +0530

    updated config

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -1,11 +1,13 @@
-import express from "express";
+import express,{Request,Response} from "express";
 import cors from "cors";
+import router from "../routes/userRoutes";
+
 
 const app = express();
 const port = 3001;
 app.use(cors());
 app.use(express.json());
-app.get("/", (req, res) => {
+app.get("/", (req:Request, res:Response) => {
   res.send("Hello World!");
 });
 
